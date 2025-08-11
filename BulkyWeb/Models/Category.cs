621706f7000dcb0bc8733df5fcbe74a4b48ae3c1
Ak <141736020+XD-SYSNC4@users.noreply.github.com>
2025-08-11 19:14:55 +0100

    Add project files.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+       
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Range(1, 30, ErrorMessage="Display order must be between 1 and 30")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
